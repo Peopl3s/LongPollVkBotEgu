e678331094b97317da9db60feefad01109e15211
Peopl3s <nikposd@mail.ru>
2018-06-10 17:11:10 +0300

    Add doc's line

--- LongPollBot12.py
+++ LongPollBot12.py
@@ -5,10 +5,31 @@ import json
 import random
 
 def answer(path):
+    '''Генерирует случайный ответ (строку) из текствого файл path'''
     with open(path,"r",encoding='windows-1251') as file:
         return random.choice(list(file.read().split('\n')))
     
 def parseJson(jsonFile='Res/content.json',jsonList='Res/listcontent.json'):
+    '''Получает данные из json-файлов, необходимых для работы бота.
+
+      jsonFile - json-файл,хранит acces_token и необходимый набор атрибутов
+      для каждой запращиваемой команды.
+      Имеет вид:
+      
+      "command":
+      { 
+	"owner_id":"o_id",  - id страницы хоста
+	"id_content":"c_id", - id файла-контента(фото,видео,аудио,документ,' ')
+	"type":"photo", - тип контента(файла) расположенного по id_content
+	"message":"Контакты" - тексовое сообщение для собеседника
+      }
+
+      jsonList - json-файл, хранит список возможных команд вида:
+      "команда":"соответсвующее заничение команды в файле jsonFile"
+
+    Оба файла необходимы для заполнения словаря,
+    посредством, которого реализовано распознавание команд пользователя ботом.
+    '''
     content={}
     try:
         jsObj = open(jsonFile, "r")
@@ -26,11 +47,13 @@ def parseJson(jsonFile='Res/content.json',jsonList='Res/listcontent.json'):
     return content
 
 def getName(vk_session,uid):
+    '''Получает имя собеcедника, с помощью метода VK API'''
      with vk_api.VkRequestsPool(vk_session) as pool:
          name=pool.method_one_param('users.get',key='user_id', values=(uid,))
      return name.result[uid][0]['first_name']
 
 def main():
+    '''Функция реализующая непосредственно работу бота'''
     content=parseJson()
     session = requests.Session()
     vk_session = vk_api.VkApi(token=content['token'])
