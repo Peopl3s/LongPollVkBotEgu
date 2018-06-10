4021e9dde4ac0188afa0140176b6040ecc46f358
Peopl3s <nikposd@mail.ru>
2018-06-10 17:21:08 +0300

    Formatted in the PEP8 style

--- LongPollBot12.py
+++ LongPollBot12.py
@@ -6,16 +6,15 @@ import random
 
 def answer(path):
     '''Генерирует случайный ответ (строку) из текствого файл path'''
-    with open(path,"r",encoding='windows-1251') as file:
+    with open(path, "r", encoding='windows-1251') as file:
         return random.choice(list(file.read().split('\n')))
     
-def parseJson(jsonFile='Res/content.json',jsonList='Res/listcontent.json'):
+def parseJson(jsonFile='Res/content.json', jsonList='Res/listcontent.json'):
     '''Получает данные из json-файлов, необходимых для работы бота.
 
       jsonFile - json-файл,хранит acces_token и необходимый набор атрибутов
       для каждой запращиваемой команды.
       Имеет вид:
-      
       "command":
       { 
 	"owner_id":"o_id",  - id страницы хоста
@@ -23,14 +22,15 @@ def parseJson(jsonFile='Res/content.json',jsonList='Res/listcontent.json'):
 	"type":"photo", - тип контента(файла) расположенного по id_content
 	"message":"Контакты" - тексовое сообщение для собеседника
       }
-
+      
       jsonList - json-файл, хранит список возможных команд вида:
       "команда":"соответсвующее заничение команды в файле jsonFile"
-
+      
     Оба файла необходимы для заполнения словаря,
     посредством, которого реализовано распознавание команд пользователя ботом.
+    
     '''
-    content={}
+    content = {}
     try:
         jsObj = open(jsonFile, "r")
         pObj = json.load(jsObj)
@@ -40,21 +40,21 @@ def parseJson(jsonFile='Res/content.json',jsonList='Res/listcontent.json'):
         print(IOErr)
     finally:
         jsObj.close()  
-    with open(jsonList,"r") as jsObj2:
-        pObj2=json.load(jsObj2)  
+    with open(jsonList, "r") as jsObj2:
+        pObj2 = json.load(jsObj2)  
     for k,v in zip(pObj2.keys(),pObj2.values()):
-         content[k]=pObj[v]
+         content[k] = pObj[v]
     return content
 
-def getName(vk_session,uid):
+def getName(vk_session, uid):
     '''Получает имя собеcедника, с помощью метода VK API'''
      with vk_api.VkRequestsPool(vk_session) as pool:
-         name=pool.method_one_param('users.get',key='user_id', values=(uid,))
+         name = pool.method_one_param('users.get', key='user_id', values=(uid,))
      return name.result[uid][0]['first_name']
 
 def main():
     '''Функция реализующая непосредственно работу бота'''
-    content=parseJson()
+    content = parseJson()
     session = requests.Session()
     vk_session = vk_api.VkApi(token=content['token'])
     longpoll = VkLongPoll(vk_session)
@@ -64,19 +64,19 @@ def main():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
             print('id{}: "{}"'.format(event.user_id, event.text), end=' ')
             attachments = []
-            msg=""
-            if(len(event.text)>0 and event.text in content):
+            msg = ""
+            if(len(event.text) > 0 and event.text in content):
                 attachments.append('{}{}_{}'.format(
                                                    content[event.text]['type'],
                                                    content[event.text]['owner_id'],
                                                    content[event.text]['id_content']
                                                    ))
                 msg=content[event.text]['message']
-            elif (len(event.text)>0 and event.text.lower() in list(open('Res/hello.txt',"r",encoding='windows-1251').read().split('\n'))):
-                firstname=getName(vk_session,event.user_id)
-                msg=answer('Res/helloansw.txt').format(firstname)
+            elif (len(event.text) > 0 and event.text.lower() in list(open('Res/hello.txt', "r", encoding='windows-1251').read().split('\n'))):
+                firstname = getName(vk_session, event.user_id)
+                msg = answer('Res/helloansw.txt').format(firstname)
             else:
-                msg='Ошибка. Список возможных команд: https://vk.com/id439685153?w=wall439685153_2'
+                msg = 'Ошибка. Список возможных команд: https://vk.com/id439685153?w=wall439685153_2'
             vk.messages.send(
                     user_id=event.user_id,
                     attachment=','.join(attachments),
